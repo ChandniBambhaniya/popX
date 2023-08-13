f539d16589497cb429f5e601c1e7edbd8797f8e6
ChandniBambhaniya <Chandnisorathiya1358@gmail.com>
2023-08-13 13:22:33 +0530

    new - setup new project

---
+++ App.js
@@ -0,0 +1,15 @@
+import {Text, View, Button} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <View style={{flexDirection: 'column-reverse', marginVertical:'10px', backgroundColor:'red'}}>
+        <Text>
+          App
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default App;
